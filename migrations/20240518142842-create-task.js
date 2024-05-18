4acca8641dffdae425a7e1999f8051e694a158c7
Zhi Qin Lee <leezhiqinuk@gmail.com>
2024-05-19 00:14:51 +0800

    Adds user and task models

---
+++ migrations/20240518142842-create-task.js
@@ -0,0 +1,52 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userID: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      parentTaskID: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Tasks',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      orderIndex: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Tasks');
+  }
+};
---
+++ models/task.js
@@ -0,0 +1,41 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Task extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Task.belongsTo(models.User, { foreignKey: 'userID', as: 'user' });
+      Task.hasMany(models.Task, { foreignKey: 'parentTaskID', as: 'subtasks' });
+      Task.belongsTo(models.Task, { foreignKey: 'id', as: 'parentTask' });
+    }
+  }
+  Task.init({
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    userID: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: User,
+        key: 'id'
+      }
+    },
+    parentTaskID: DataTypes.INTEGER,
+    title: DataTypes.STRING,
+    status: DataTypes.STRING,
+    orderIndex: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Task',
+  });
+  return Task;
+};
