4561c1490610190f9fb79afb008dc7cafef7f879
leezhiqin96 <leezhiqinuk@gmail.com>
2024-05-20 17:55:32 +0800

    Adds navbar template

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import './styles/common.scss';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
--- src/layout.js
+++ src/layout.js
@@ -1,4 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import './styles/common.scss';
+import './styles/navbar.scss';
+
+import SideNavBar from "./components/layout/sideNavBar.component";
+
+const navbarElement = document.getElementById('side-nav');
+if (navbarElement) {
+    const navbar = ReactDOM.createRoot(navbarElement);
+    navbar.render(<SideNavBar />);
+}
+
 // Adds navbar component here
