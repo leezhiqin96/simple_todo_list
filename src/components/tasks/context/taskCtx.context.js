d9e3d636691507c51a9a971fa2f8b3f9ad949299
Lee Zhi Qin <leezhiqinuk@gmail.com>
2024-05-22 20:35:24 +0800

    Update subtask reducer

--- src/components/tasks/context/taskCtx.context.js
+++ src/components/tasks/context/taskCtx.context.js
@@ -46,6 +46,21 @@ const tasksReducer = (state, action) => {
             : task
         ),
       };
+    case 'UPDATE_SUBTASK':
+      return {
+        ...state,
+        tasks: state.tasks.map(task => {
+          if (task.id === action.payload.parentTaskID) {
+            return {
+              ...task,
+              subtasks: task.subtasks.map(subtask =>
+                subtask.id === action.payload.updatedSubtask.id ? action.payload.updatedSubtask : subtask
+              )
+            };
+          }
+          return task;
+        }),
+      };
     default:
       throw new Error(`Unhandled action type: ${action.type}`);
   }
@@ -102,15 +117,32 @@ export default function TaskContextProvider({ children }) {
     }
   };
 
-  const updateTask = async (rowID, field, value) => {
+  const addSubTask = async (taskID, title) => {
+    try {
+      const addTaskResult = await axios.post(`/users/${userID}/tasks/${taskID}`, { taskTitle: title });
+      dispatchTasks({ type: 'ADD_SUB_TASK', payload: { taskID, subtask: addTaskResult.data.newTask } });
+    } catch (error) {
+      console.error('Error adding task:', error);
+    }
+  }
+
+  const updateTask = async (rowID, field, value, parentTaskID = null) => {
     const payload = { id: rowID, field, value };
+
     try {
       const updateTaskResult = await axios.put(`/users/${userID}/tasks/${rowID}`, payload);
-      dispatchTasks({ type: 'UPDATE_TASK', payload: updateTaskResult.data.updatedTask });
+      const updatedTask = updateTaskResult.data.updatedTask;
+
+      if (parentTaskID) {
+        dispatchTasks({ type: 'UPDATE_SUBTASK', payload: { parentTaskID, updatedSubtask: updatedTask } });
+      } else {
+        dispatchTasks({ type: 'UPDATE_TASK', payload: updatedTask });
+      }
     } catch (error) {
       console.error('Error updating task:', error);
     }
   };
+
 
   const deleteTasks = (tasksIDList) => {
     return new Promise((resolve, reject) => {
@@ -128,16 +160,6 @@ export default function TaskContextProvider({ children }) {
   const selectTask = (taskID) => {
     dispatchTasks({ type: 'SELECT_TASK', payload: taskID });
   }
-
-  const addSubTask = async (taskID, title) => {
-    try {
-      const addTaskResult = await axios.post(`/users/${userID}/tasks/${taskID}`, { taskTitle: title });
-      dispatchTasks({ type: 'ADD_SUB_TASK', payload: { taskID, subtask: addTaskResult.data.newTask } });
-    } catch (error) {
-      console.error('Error adding task:', error);
-    }
-  }
-
 
   const ctxValue = {
     userTasks,
