d0cad4cc5e28d067bd607e99f0a7a0065ae1b78e
leezhiqin96 <leezhiqinuk@gmail.com>
2024-06-06 15:02:54 +0800

    Modified production db initialisation

--- models/index.js
+++ models/index.js
@@ -11,8 +11,17 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (process.env.DATABASE_URL) {
+  // For production, use the DATABASE_URL environment variable
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // For self-signed certificates
+      }
+    }
+  });
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
