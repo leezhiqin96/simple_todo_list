4a9c37e0595b586ab15f27501366a0e7e8def906
leezhiqin96 <leezhiqinuk@gmail.com>
2024-06-06 15:10:25 +0800

    Edit config.js for production var

--- config/config.js
+++ config/config.js
@@ -11,18 +11,14 @@ module.exports = {
     logging: false
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME_PROD,
-    host: process.env.DB_HOST,
+    // Use DATABASE_URL from Heroku environment variables
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
-    port: process.env.DB_PORT_PROD,
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    },
-    logging: false
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // For self-signed certificates
+      }
+    }
   }
 };
--- models/index.js
+++ models/index.js
@@ -11,17 +11,8 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 let sequelize;
-if (process.env.DATABASE_URL) {
-  // For production, use the DATABASE_URL environment variable
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false // For self-signed certificates
-      }
-    }
-  });
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
